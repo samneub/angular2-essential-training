fdcb65f321bfeea7bbd0ba165a4588ebf433b5f4
Sam Neubauer <srneubauer@spscommerce.com>
2017-12-05 15:43:57 -0600

    Up to HTTP section

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,25 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemFormComponent } from './media-item.form.component';
+import { MediaItemService } from './media-item.service';
+import { lookupListToken, lookupLists } from './providers';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
+    MediaItemFormComponent,
     FavoriteDirective,
     CategoryListPipe
+  ],
+  providers: [
+    MediaItemService,
+    { provide: lookupListToken, useValue: lookupLists }
   ],
   bootstrap: [
     AppComponent
--- app/media-item.form.component.ts
+++ app/media-item.form.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Validators, FormBuilder } from "@angular/forms";
+
+import { MediaItemService } from './media-item.service';
+import { lookupListToken } from './providers';
 
 @Component({
   selector: 'mw-media-item-form',
-  templateUrl: 'media-item.form.component.html',
-  styleUrls: ['media-item.form.component.css']
+  templateUrl: 'app/media-item.form.component.html',
+  styleUrls: ['app/media-item.form.component.css']
 })
 export class MediaItemFormComponent {
+  form;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private mediaItemService: MediaItemService,
+    @Inject(lookupListToken) public lookupLists) {}
+
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      medium: this.formBuilder.control('Movies'),
+      name: this.formBuilder.control('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: this.formBuilder.control('default'),
+      year: this.formBuilder.control('', this.yearValidator)
+    });
+  }
+
+  yearValidator(control): any {
+    if(control.value.trim().length === 0) {
+      return null;
+    }
+    let year: number = parseInt(control.value);
+    let minYear: number = 1800;
+    let maxYear: number = 2100;
+    if(year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { 'year': {
+        min: minYear,
+        max: maxYear
+      } };
+    }
+  }
+
+  onSubmit(mediaItem) {
+    this.mediaItemService.add(mediaItem);
+  }
 }
