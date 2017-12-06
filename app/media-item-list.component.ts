dab89a1ff51ec6459df3474f934a4ee6cfcbdeb1
Sam Neubauer <srneubauer@spscommerce.com>
2017-12-06 10:17:58 -0600

    Broken in http

--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -8,15 +8,24 @@ import { MediaItemService } from './media-item.service';
   styleUrls: ['app/media-item-list.component.css']
 })
 export class MediaItemListComponent {
-  mediaItems;
+  medium = '';
+  mediaItems = [];
 
   constructor(private mediaItemService: MediaItemService) {}
 
   ngOnInit() {
-    this.mediaItems = this.mediaItemService.get();
+    this.getMediaItems(this.medium);
   }
 
   onMediaItemDelete(mediaItem) { 
     this.mediaItemService.delete(mediaItem);
+  }
+
+  getMediaItems(medium) {
+    this.medium = medium;
+    this.mediaItemService.get(medium)
+      .subscribe(mediaItems => {
+        this.mediaItems = mediaItems;
+      });
   }
 }
--- app/media-item.service.ts
+++ app/media-item.service.ts
@@ -1,6 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
 export class MediaItemService {
-  get() {
-    return this.mediaItems;
+
+  constructor(private http: Http) {}
+  
+  
+  get(medium) {
+    let searchParams = new URLSearchParams();
+    searchParams.append('medium', medium);
+    return this.http.get('mediaitems', { search: searchParams })
+      .map(response => {
+        return response.json().mediaItems;
+      });
   }
 
   add(mediaItem) {
